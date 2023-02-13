00ea70adefa9ee76525c1a381cf23aeb9f2c3da2
Anshuman <anshuman.si22@gmail.com>
2023-02-13 13:49:38 +0530

    implementing dot functionality in slider

--- script.js
+++ script.js
@@ -295,6 +295,99 @@ threshold:0,
 rootMargin:'200px',
 })
 
-imgTarget.forEach((img)=>imgObserver.observe(img))
+imgTarget.forEach((img)=>imgObserver.observe(img));
+
+
+//-----------------------------
+//-----------------------------
+//slider components
+//-----------------------------
+//-----------------------------
+
+const slides=document.querySelectorAll('.slide');
+
+const btnLeft=document.querySelector('.slider__btn--left');
+const btnRight=document.querySelector('.slider__btn--right');
+const dotContainer=document.querySelector('.dots')
+
+let currentSlide=0;
+const maxSlide=slides.length;
+//setting up the initial position of the slides
+//0% 100% 200% 300%
+// slides.forEach((s,i)=>{
+//   s.style.transform=`translateX(${100*i}%)`
+// })
+
+const createDots=function(){
+  slides.forEach(function(_,i){
+    dotContainer.insertAdjacentHTML('beforeend',
+    `<button class="dots__dot" data-slide="${i}"></button>`)
+  
+  })
+}
+
+createDots();
+
+const goToSlide=function(slide){
+  slides.forEach((s,i)=>s.style.transform=`translateX(${100*(i-slide)}%)`)
+
+}
+goToSlide(0)
+
+const nextSlide=function(){
+  
+  if(currentSlide===(maxSlide-1)){
+    currentSlide=0
+  }else{
+    currentSlide++;
+  }
+  goToSlide(currentSlide)
+}
+
+ const prevSlide=function(){
+  if(currentSlide===0){
+    currentSlide=maxSlide-1;
+  }else{
+    currentSlide--;
+  }
+  goToSlide(currentSlide);
+ }
+
+//to go to next slide
+
+
+btnRight.addEventListener('click',nextSlide);
+btnLeft.addEventListener('click',prevSlide);
+
+document.addEventListener('keydown',function(e){
+  // if(e.key==='ArrowLeft'){
+  //   prevSlide();
+  // }else if(e.key==='ArrowRight'){
+  //   nextSlide();
+  // }else{
+  //   return;
+  // }
+  
+  //Same functionalty using short circuting
+   
+  e.key==='ArrowLeft' && prevSlide();
+  e.key==='ArrowRight' && nextSlide();
+  
+
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
